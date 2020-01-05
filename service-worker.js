10fd0f5431dcd516da13a7b700d09e68adc5ebc4
acbeers <andrew_beers@yahoo.com>
2020-01-05 02:55:53 +0000

    Deploying to gh-pages from  b0d460e67f40b9c40009ca548cdf63a4eb8feeeb

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a19a5cf6f56ce7ace2d407d9d6964fe6.js"
+  "./precache-manifest.482ce8a660417b4ce1b1e4ba692e3803.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
